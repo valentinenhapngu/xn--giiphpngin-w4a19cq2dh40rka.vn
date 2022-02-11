dd58376f236d45644ac6f3489fd806459ce5e3aa
thanhnh <valentine.nhapngu@gmail.com>
2022-02-11 15:32:18 -0700

    add index.php to branch website

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo WebApp</title>
+</head>
+<body>
+    Say Hello world! 
+    <hr>
+    <?php
+    echo "say Hello PHP"
+    ?>
+</body>
+</html>
