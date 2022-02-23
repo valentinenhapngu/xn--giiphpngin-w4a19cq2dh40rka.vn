6e53d315da773fd086b40526151e1d9dd4f796a0
thanhnh <valentine.nhapngu@gmail.com>
2022-02-23 14:32:33 +0700

    Change to TodoList v.0.1.0

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 xn--giiphpngin-w4a19cq2dh40rka.vn
 commit from vscode
 commit 2 checkout to website 
+
+#22/02/23 : Change to ToDoList on xn--giiphpngin-w4a19cq2dh40rka.vn
--- index.php
+++ index.php
@@ -5,25 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Demo WebApp</title>
+    <script src="script.js"></script>
+    <title>To DO List</title>
 </head>
 <body>
-    Say Hello world! 
-    <hr>
     <?php
-    echo "say Hello PHP"
+    echo "<p>xn--giiphpngin-w4a19cq2dh40rka.vn<p>"
     ?>
     <hr>
-    <div class="wrap-note">
-        <div class="note-todo">
-
-        </div>
-        <div class="note-doing">
-
-        </div>
-        <div class="note-done">
-
-        </div>
-    </div>
+    
 </body>
 </html>
