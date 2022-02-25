57cd7258c8840dbfd43f4330f78078a114b0f77c
thanhnh <thanhnh@topcv.vn>
2022-02-25 16:52:13 +0700

    x

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php 
-    require "css_link.php"; 
-    require "script_link.php";
-    echo $css_link;
-    echo $script_link;
+    <?php
+    // require "css_link.php"; 
+    // require "script_link.php";
+    // echo $css_link;
+    // echo $script_link;
     ?>
-    
+    <link rel="stylesheet" href="font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_listview.css">
+    <link rel="stylesheet" href="style_treeview.css">
+    <link rel="stylesheet" href="style_aboutview.css">
+    <link rel="stylesheet" href="style_about_card.css">
+    <link rel="stylesheet" href="style_about_accordion.css">
+    <script src="script.js"></script>
     <title>To DO List</title>
 </head>
 
